7b4d19fd06ac3f9f8db33cd3488abb52152f2f9d
TamTam <tttamarita@yahoo.com>
2023-02-05 18:41:35 +0100

    Change padding in footer

--- app.js
+++ app.js
@@ -21,7 +21,7 @@ const contactTitle = "Budimo u kontaktu";
 
 const app = express();
 
-const PORT = process.env.PORT || 8080;
+const PORT = process.env.PORT || 8080; // u railway app PORT 8080
 
 app.set('view engine', 'ejs');
 
